d3fc27759ddab206f39586458a513bd3fe208961
Gaurav Patil <patil6624@Gauravs-MacBook-Air.local>
2022-10-16 22:55:43 +0530

    Add Evenodd program file

---
+++ EvenOdd.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace evenodd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            Console.Write("Enter a Number : ");
+            i = int.Parse(Console.ReadLine());
+            if (i % 2 == 0)
+            {
+                Console.Write("Enter a Number is an Even Number");
+                Console.Read();
+            }
+            else
+            {
+                Console.Write("Enter a Number is an Odd Number");
+                Console.Read();
+            }
+        }
+    }
+}
